603631d2d13bf915925ff936811c492fcfbda6e8
bailappa <bailappa@ix.lentra.ai>
2023-03-01 11:32:25 +0530

    Commit version v13

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://gw-stack-webappdeploymentbucket-knyrdwfsflox/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://gw-stack-webappdeploymentbucket-1c0b1zi8gzoie/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
